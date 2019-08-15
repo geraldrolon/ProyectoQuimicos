bc5fb0b10cf4d8527a03fdb3e48a9a07f900cdba
geraldrolon <54126302+geraldrolon@users.noreply.github.com>
2019-08-15 11:53:55 -0500

    Initial commit

---
+++ ProyectoQuimicos/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProyectoQuimicos.Data;
+
+[assembly: HostingStartup(typeof(ProyectoQuimicos.Areas.Identity.IdentityHostingStartup))]
+namespace ProyectoQuimicos.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
